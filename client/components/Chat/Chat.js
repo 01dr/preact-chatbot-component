5351ed31b65d414122dd56bde04756095078c528
Space Invader <amazing.space.invader@gmail.com>
2016-12-07 13:28:09 +0300

    add chat header

--- client/components/Chat/Chat.js
+++ client/components/Chat/Chat.js
@@ -4,13 +4,17 @@ import { connect } from 'preact-redux';
 import classNames from 'classnames/bind';
 import s from './chat.pcss';
 
+import ChatHeader from './components/ChatHeader/ChatHeader.js';
+
 class Chat extends Component {
 	render() {
 		const { dispatch, open } = this.props;
 		const st = classNames.bind(s);
 
 		return (
-			<div className={ st({ chat: true, open }) }>hello, handsome guy!</div>
+			<div className={ st({ chat: true, open }) }>
+				<ChatHeader/>
+			</div>
 		);
 	}
 }
---
+++ client/components/Chat/components/ChatHeader/ChatHeader.js
@@ -0,0 +1,21 @@
+import { h, Component } from 'preact';
+import { connect } from 'preact-redux';
+
+import s from './chatHeader.pcss';
+
+import barney from './images/barney.png';
+
+class ChatHeader extends Component {
+	render() {
+		return (
+			<div className={s.header}>
+				<div className={s.avatar}>
+					<img src={barney}/>
+				</div>
+				<div className={s.name}>Бробот</div>
+			</div>
+		);
+	}
+}
+
+export default connect(() => ({}))(ChatHeader);
