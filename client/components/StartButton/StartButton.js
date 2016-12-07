98b958eceec7b56d45b4700c11482fddde449a5b
Space Invader <amazing.space.invader@gmail.com>
2016-12-07 14:56:46 +0300

    start button state

--- client/components/StartButton/StartButton.js
+++ client/components/StartButton/StartButton.js
@@ -3,23 +3,32 @@ import { connect } from 'preact-redux';
 
 import { toggleChatWindow } from '../../actions/ChatActions.js';
 
+import classNames from 'classnames/bind';
 import s from './startButton.pcss';
 
 import Chat from '../Chat/Chat.js';
 
 class StartButton extends Component {
 	render() {
-		const {dispatch} = this.props;
+		const {dispatch, open} = this.props;
+		const st = classNames.bind(s);
 
 		return (
 			<div className={s.broBot}>
 				<Chat/>
 				<button
 					onClick={() => dispatch(toggleChatWindow())}
-					className={s.startButton} />
+					className={ st({startButton: true, open}) }>
+
+					<div className={s.front}/>
+					<div className={s.back}/>
+
+				</button>
 			</div>
 		);
 	}
 }
 
-export default connect(() => ({}))(StartButton);
+export default connect(state => ({
+	open: state.chat
+}))(StartButton);
